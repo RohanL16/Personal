b08c17bee7991d5f869b06636201e6d0412cb42a
RohanL16 <RohanL16@users.noreply.github.com>
2017-12-21 14:04:35 -0800

    Delete inject.js

--- inject.js
+++
@@ -1,8 +0,0 @@
-HTMLFormElement.prototype._nativeSubmit = HTMLFormElement.prototype.submit;
-HTMLFormElement.prototype.submit = function () {
-  var submitEvent = document.createEvent("HTMLEvents");
-  submitEvent.initEvent("submit", true, true);
-  if (this.dispatchEvent(submitEvent)) {
-    this._nativeSubmit.apply(this, arguments);
-  }
-};
